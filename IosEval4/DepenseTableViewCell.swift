b136b52666568f7fc6177af1a56371037bd0bf9b
BillyVII <gabrielasteria@gmail.com>
2022-10-10 09:27:02 +0200

    mise en place de CoreData

---
+++ IosEval4/DepenseTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  DepenseTableViewCell.swift
+//  IosEval4
+//
+//  Created by Student09 on 10/10/2022.
+//
+
+import UIKit
+
+class DepenseTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var depense: UIView!
+    @IBOutlet weak var price: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- IosEval4/ViewController.swift
+++ IosEval4/ViewController.swift
@@ -9,11 +9,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var depenseTableView: UITableView! {
+        didSet {
+            depenseTableView.register(UINib(nibName: "DepenseTableViewCell", bundle: .main), forCellReuseIdentifier: "DepenseTableViewCell")
+            
+        }
+    }
+    
+    var depenseTab = [Expense]()
+    
+    var manageObjectContext = AppDelegate.shared.persistentContainer.viewContext
+     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        loadExpenseFromCoreData()
     }
 
+    func loadExpenseFromCoreData() {
+        let request = Expense.fetchRequest()
+        
+        let orderByName = NSSortDescriptor(key: name, ascending: true)
+        let orderByDate = NSSortDescriptor(key: date, ascending: true)
+        
+        do {
+            depenseTab = try manageObjectContext.fetch(request)
+        } catch {
+            print("can't access Core Data")
+        }
+        
+        depenseTableView.reloadData()
+    
+    }
+    
+    func saveData() {
+        do {
+            try manageObjectContext.save()
+        }catch {
+            fatalError("oops")
+        }
+    }
 
+    @IBAction func addDepense(_ sender: Any) {
+    }
 }
 
